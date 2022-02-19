0d5084c92e4b0dca4b7b321e2a09bad1b666b09a
Johnathan Shields <Johnathan.Shields@buhlergroup.com>
2022-02-18 22:11:07 -0600

    returning values from department with console.table

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO department
+    (name)
+VALUES
+    ('Automation');
+
+INSERT INTO role
+    (title, salary, department_id)
+VALUES
+    ('Automation Engineer', 45000, 44);
+
+INSERT INTO employee
+    (first_name, last_name, role_id, manager_id)
+VALUES
+    ('Johnathan', 'Shields', 1, 1);
--- server.js
+++ server.js
@@ -1,7 +1,40 @@
 const db = require('./db/connection');
+const inquirer = require('inquirer');
+const cTable = require('console.table');
+
+// questions to be asked in the CLI
+const questions = [
+    {
+        type: 'list',
+        name: 'toDo',
+        message: 'What would you like to do?',
+        choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role']
+    }
+]
+
+// function to start asking inquirer questions
+const askQuestions = () => {
+    return inquirer
+    .prompt(questions)
+    .then(data => {
+        if (data.toDo === 'View all departments'){
+            const sql = `SELECT * FROM department;`;
+
+            db.query(sql, (err, row) => {
+                if (err) {
+                    console.log(err)
+                    return;
+                }
+                console.table(row)
+            })
+        }
+        
+    })
+}
 
 // start server after DB connection
 db.connect(err => {
     if (err) throw err;
     console.log('Database connected.');
+    askQuestions();
 })
