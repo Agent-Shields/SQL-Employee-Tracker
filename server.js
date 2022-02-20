01b6768ba0ae1eea4f069b05314fb08ee43257b8
Johnathan Shields <Johnathan.Shields@buhlergroup.com>
2022-02-20 10:34:06 -0600

    refactored for switch cases

--- server.js
+++ server.js
@@ -12,54 +12,102 @@ const questions = [
     }
 ]
 
+// sql query to view all departments
+queryDept = () => {
+    const sql = `SELECT * FROM department;`;
+            
+    db.query(sql, (err, deptTable) => {
+        if (err) {
+            console.log(err)
+            return;
+        }
+        console.table(deptTable);
+        return;
+    })
+};
+
+// sql query to view all roles
+queryRole = () => {
+    const sql = `SELECT * FROM role`;
+
+    db.query(sql, (err, roleTable) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.table(roleTable);
+        return;
+    })
+};
+
+// sql query to view all employees
+queryEmployees = () => {
+    const sql = `SELECT * FROM employee`
+
+    db.query(sql, (err, employeeTable) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.table(employeeTable);
+        return;
+    })
+};
+
+// sql query to add department
+addDept = () => {
+    // get user input for department name
+    // add department to db based off user input
+}
+
+// sql query to add role
+addRole = () => {
+    // get user input for role information
+    // add role to db based off user input
+}
+
+// sql query to add employee
+addEmployee = () => {
+    // get user input for employee information
+    // add employee to db based off user input
+}
+
+// sql query to update employee role
+updateRole = () => {
+
+}
 // function to start asking inquirer questions
 const askQuestions = () => {
     inquirer
     .prompt(questions)
     .then(data => {
-        if (data.toDo === 'View all departments') {
-            const sql = `SELECT * FROM department;`;
+        let choices = data.toDo;
+        // console.log(choices);
+        switch (choices) {
+            case 'View all departments':
+                queryDept();
+                break;
 
-            db.query(sql, (err, deptTable) => {
-                if (err) {
-                    console.log(err)
-                    return;
-                }
-                console.table(deptTable);
-            })
-        } else if (data.toDo === 'View all roles') {
-            const sql = `SELECT * FROM role`;
+            case 'View all roles':
+                queryRole();
+                break;
 
-            db.query(sql, (err, roleTable) => {
-                if (err) {
-                    console.log(err);
-                    return;
-                }
-                console.table(roleTable)
-            })
-        } else if (data.toDo === 'View all employees') {
-            const sql = `SELECT * FROM employee`
-
-            db.query(sql, (err, employeeTable) => {
-                if (err) {
-                    console.log(err);
-                    return;
-                }
-                console.table(employeeTable)
-            })
-        } else if (data.toDo === 'Add a department') {
-            // get user input for department name
-            // add department to db based off user input
-        } else if (data.toDo === 'Add a role') {
-            // get user input for role information
-            // add role to db based off user input
-        } else if (data.toDo === 'Add an employee') {
-            // get user input for employee information
-            // add employee to db based off user input
-        } else if (data.toDo === 'Update an employee role') {
-            
+            case 'View all employees':
+                queryEmployees();
+                break;
+            case 'Add a department':
+                addDept();
+                break;
+            case 'Add a role':
+                addRole();
+                break;
+            case 'Add an employee':
+                addEmployee();
+                break;
+            case 'Update an employee role':
+                updateRole();
+                break;
         }
-        
     })
 }
 
